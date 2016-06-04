a3146d80b2076e970f4ef7a5e9927c8d34988c54
ByvshevS <byvshev.sergey@gmail.com>
2016-06-04 13:33:33 +0300

    first commit

--- src/main/java/juhesequa/Main.java
+++ src/main/java/juhesequa/Main.java
@@ -2,11 +2,15 @@ package juhesequa;
 
 import static spark.Spark.get;
 import static spark.SparkBase.port;
+import static spark.SparkBase.staticFileLocation;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		
 	    port(Integer.valueOf(System.getenv("PORT")));
+
+	    staticFileLocation("/public");
 
         get("/hello", (req, res) -> "Hello World");
 	}
